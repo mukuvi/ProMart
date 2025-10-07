b69ac6c250a22db1b62c4cb78caad744f0c628a1
mukuvi <jamesmngandu@gmail.com>
2025-10-07 18:21:01 +0300

    ProMart

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,9 @@ import mongoose, { modelNames } from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Successfully connectedto mongoDB :)`);
   } catch (error) {
     console.error(`ERROR: ${error.message}`);
   }
 };
+export default connectDB;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => console.log(`Server running on port : ${PORT}`));
