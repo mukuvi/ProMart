1678cd418c8479b50b36140ead2d0e47c54ca73b
mukuvi <jamesmngandu@gmail.com>
2025-10-07 23:57:01 +0300

    Pro Mart

--- server/server.js
+++ server/server.js
@@ -2,7 +2,10 @@ import express from "express";
 import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+dotenv.config();
 const PORT = process.env.PORT || 5000;
+connectDB();
 
 const app = express();
 app.get("/api", (req, res) => {
