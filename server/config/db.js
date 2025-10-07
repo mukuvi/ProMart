a54ad7e6c86983bf9ee09700940da9c16caa89d6
mukuvi <jamesmngandu@gmail.com>
2025-10-07 23:02:21 +0300

    Pro Mart

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
-import mongoose, { modelNames } from "mongoose";
+import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
--- server/server.js
+++ server/server.js
@@ -5,5 +5,7 @@ import cookieParser from "cookie-parser";
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
+app.get("/api", (req, res) => {
+  res.send("Hello sir");
+});
 app.listen(PORT, () => console.log(`Server running on port : ${PORT}`));
