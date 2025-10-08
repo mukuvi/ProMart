3526d60b5166965d266b3a9e8050c9612ea616d1
mukuvi <jamesmngandu@gmail.com>
2025-10-08 15:43:22 +0300

    Pro Mart

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,7 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
 import User from "../models/userModel.js";
 
-const createUser = asyncHandler;
+const createUser = asyncHandler(async (req, res) => {
+  res.send("Hello");
+});
+export { createUser };
